0325fe46d102102f060b28845d04be8eb6ddf819
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-04-12 14:55:04 +0700

    COMPLETE: Read GPS data over UART0 and print it to UART1

--- Device/project_template/IP/Example/main.c
+++ Device/project_template/IP/Example/main.c
@@ -31,11 +31,8 @@
 #include "ht32_board.h"
 #include "ht32f5xxxx_usart.h"
 
-
-
 #define USART_FLAG_TXE ((u16)0x0080)
 #define USART_FLAG_RXNE ((uint16_t)0x0020)
-
 
 /** @addtogroup Project_Template Project Template
   * @{
@@ -62,6 +59,9 @@ void UxART1_TxSend(u16 Data);
 void UxART1_TxTest(void);
 void UxART1_RxTest_Block(void);
 void UxART1_RxTest_NonBlock(void);
+void UxART0_Configuration(void);
+void UxART0_RxTest_Block(void);
+void UxART0_RxTest_NonBlock(void);
 
 #if (ENABLE_CKOUT == 1)
 void CKOUTConfig(void);
@@ -88,6 +88,7 @@ int main(void)
   GPIO_Configuration();               /* GPIO Related configuration                                         */
   //RETARGET_Configuration();           /* Retarget Related configuration                                     */
 	UxART1_Configuration();
+	UxART0_Configuration();
 
   HT32F_DVB_LEDInit(HT_LED1);
   HT32F_DVB_LEDInit(HT_LED2);
@@ -120,6 +121,7 @@ int main(void)
     #else // Non-Blocking mode
     {
       UxART1_RxTest_NonBlock();    // Return if no UxART data, LED can toggle by "LED_Demo()".
+			UxART0_RxTest_Block();
     }
     #endif
   }
@@ -274,7 +276,7 @@ void GPIO_Configuration(void)
   ***********************************************************************************************************/
 void UxART1_Configuration(void)
 {
-	CKCU_PeripClockConfig_TypeDef CKCUClock = {{ 0 }}; // Set all the fields to zero, which means that no peripheral clocks are enabled by default.
+	CKCU_PeripClockConfig_TypeDef CKCUClock; // Set all the fields to zero, which means that no peripheral clocks are enabled by default.
 
 	{/* Enable peripheral clock of AFIO, UxART                                                                 */
 	CKCUClock.Bit.AFIO = 1;
@@ -380,6 +382,77 @@ void UxART1_RxTest_NonBlock(void)
   }
 }
 
+void UxART0_Configuration(void)
+{
+	CKCU_PeripClockConfig_TypeDef CKCUClock; // Set all the fields to zero, which means that no peripheral clocks are enabled by default.
+
+	{/* Enable peripheral clock of AFIO, UxART                                                                 */
+	CKCUClock.Bit.AFIO = 1;
+	CKCUClock.Bit.PB = 1;
+	CKCUClock.Bit.UART0 = 1;
+	CKCU_PeripClockConfig(CKCUClock, ENABLE);
+	}
+	
+	/* Turn on UxART Rx internal pull up resistor to prevent unknow state                                     */
+  GPIO_PullResistorConfig(HT_GPIOB, GPIO_PIN_8, GPIO_PR_UP);
+	
+	/* Config AFIO mode as UxART function.                                                                    */
+  AFIO_GPxConfig(GPIO_PB, AFIO_PIN_7, AFIO_FUN_USART_UART);
+  AFIO_GPxConfig(GPIO_PB, AFIO_PIN_8, AFIO_FUN_USART_UART);
+	
+	{
+    /* UxART configured as follow:
+          - BaudRate = 115200 baud
+          - Word Length = 8 Bits
+          - One Stop Bit
+          - None parity bit
+    */
+
+    /* !!! NOTICE !!!
+       Notice that the local variable (structure) did not have an initial value.
+       Please confirm that there are no missing members in the parameter settings below in this function.
+    */
+    USART_InitTypeDef USART_InitStructure = {0};
+    USART_InitStructure.USART_BaudRate = 9600;
+    USART_InitStructure.USART_WordLength = USART_WORDLENGTH_8B;
+    USART_InitStructure.USART_StopBits = USART_STOPBITS_1;
+    USART_InitStructure.USART_Parity = USART_PARITY_NO;
+    USART_InitStructure.USART_Mode = USART_MODE_NORMAL;
+    USART_Init(HT_UART0, &USART_InitStructure);
+  }
+	
+	/* Enable UxART Tx and Rx function                                                                        */
+  USART_TxCmd(HT_UART0, ENABLE);
+  USART_RxCmd(HT_UART0, ENABLE);
+}
+
+void UxART0_RxTest_Block(void)
+{
+	u16 uData;
+
+  /* Waits until the Rx FIFO/DR is not empty then get data from them                                        */
+  while (USART_GetFlagStatus(HT_UART0, USART_FLAG_RXDR) == RESET);
+  uData = USART_ReceiveData(HT_UART0);
+
+  #if 1 // Loop back Rx data to Tx for test
+  UxART1_TxSend(uData);
+  #endif
+}
+
+void UxART0_RxTest_NonBlock(void)
+{
+	u16 uData;
+
+  /* Waits until the Rx FIFO/DR is not empty then get data from them                                        */
+  if (USART_GetFlagStatus(HT_UART0, USART_FLAG_RXDR) == SET)
+  {
+    uData = USART_ReceiveData(HT_UART0);
+
+    #if 1 // Loop back Rx data to Tx for test
+    UxART1_TxSend(uData);
+    #endif
+  }
+}
 
 #if (HT32_LIB_DEBUG == 1)
 /*********************************************************************************************************//**
