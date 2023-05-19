a50eca06cdf81c5f6bb103cb177c145cacb150fa
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-20 01:12:02 +0700

    COMPLETE: Run App oK when add amplify.configure

--- App/tracking-app/App.js
+++ App/tracking-app/App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
 
+Amplify.configure(awsconfig);
 export default function App() {
   return (
     <View style={styles.container}>
