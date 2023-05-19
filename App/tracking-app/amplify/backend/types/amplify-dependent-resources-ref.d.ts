59b27adf052d8175f8aa04106439883c2460a46e
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-20 01:18:23 +0700

    CHECKPOINT: Add auth with Username and push successfully

--- App/tracking-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ App/tracking-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "trackingappaa6d2694": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
