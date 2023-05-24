ccc6a4643d28ce8f26eca17e27d7a5e772b02d97
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-24 22:22:20 +0700

    CHECKPOINT: Init new repo in new way
    import { Amplify } from 'aws-amplify'; not import Amplify from 'aws-amplify';

---
+++ App/tracker-app/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
