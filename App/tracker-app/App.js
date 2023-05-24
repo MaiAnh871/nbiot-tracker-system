5b43528fa2a23d6320accdbd6129bb6842b97d17
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-24 23:12:35 +0700

    CHECKPOINT: Add withAuthenticator

--- App/tracker-app/App.js
+++ App/tracker-app/App.js
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './src/aws-exports';
-
+import { withAuthenticator } from 'aws-amplify-react-native';
 Amplify.configure(awsconfig);
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -22,3 +22,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
