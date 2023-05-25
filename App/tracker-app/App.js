7d725f71b5450c00e321bd089e08ab4802df9f4b
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-25 21:52:35 +0700

    CHECKPOINT: Add signOut()

--- App/tracker-app/App.js
+++ App/tracker-app/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import awsconfig from './src/aws-exports';
 import { withAuthenticator } from 'aws-amplify-react-native';
 Amplify.configure(awsconfig);
 
 function App() {
+  Auth.signOut();
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
