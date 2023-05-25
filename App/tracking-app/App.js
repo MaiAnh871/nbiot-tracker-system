4215f914d5f2c8dbcdffa232d95506da196df992
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-25 23:18:03 +0700

    COMPLETE: Delete Phone field and run OK

--- App/tracking-app/App.js
+++ App/tracking-app/App.js
@@ -25,4 +25,39 @@ const styles = StyleSheet.create({
   },
 });
 
-export default withAuthenticator(App);
+const signUpConfig = {
+  header: "Create a new account",
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: "Full name",
+      key: "name",
+      required: true,
+      displayOrder: 1,
+      type: "string",
+    },
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 2,
+      type: "string",
+    },
+    {
+      label: "Username",
+      key: "preferred_username",
+      required: true,
+      displayOrder: 3,
+      type: "string",
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 4,
+      type: "password",
+    },
+  ],
+};
+
+export default withAuthenticator(App, { signUpConfig });
