9225747dad77fc5d8dcae54aea8c8b85103e417c
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-26 23:07:07 +0700

    CHECKPOINT: Delete for initial new prj

--- App/tracker-app/App.js
+++ App/tracker-app/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Amplify, Auth } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsconfig from './src/aws-exports';
-import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react-native';
 Amplify.configure(awsconfig);
 
 function App() {
-  Auth.signOut();
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -24,46 +22,4 @@ const styles = StyleSheet.create({
   },
 });
 
-const signUpConfig = {
-  header: "My Customized Sign Up",
-  hideAllDefaults: true,
-  signUpFields: [
-    {
-      label: "Full name",
-      key: "name",
-      required: true,
-      displayOrder: 1,
-      type: "string",
-    },
-    {
-      label: "Email",
-      key: "email",
-      required: true,
-      displayOrder: 2,
-      type: "string",
-    },
-    {
-      label: "Username",
-      key: "preferred_username",
-      required: true,
-      displayOrder: 3,
-      type: "string",
-    },
-    {
-      label: "Password",
-      key: "password",
-      required: true,
-      displayOrder: 4,
-      type: "password",
-    },
-  ],
-};
-
-const customTheme = {
-  ...AmplifyTheme,
-  button: {
-    ...AmplifyTheme.button,
-    //backgroundColor: 'blue',
-    borderRadius: 10,
-  }}
-export default withAuthenticator(App, { signUpConfig, theme: customTheme });
+export default App;
