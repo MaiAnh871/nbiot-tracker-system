a9b6c0f6ebba87f5c5345ffb5fc6469e11e109cb
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-27 00:19:33 +0700

    COMPLETE: Show UI

--- App/tracker-app/App.js
+++ App/tracker-app/App.js
@@ -1,24 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Navigation from './src/navigation';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './src/aws-exports';
+
 Amplify.configure(awsconfig);
 
-function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <Navigation />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F9FBFC',
   },
 });
 
