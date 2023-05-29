501ef16829b35125c086f745053abb3c587a7b7f
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-29 17:38:08 +0700

    CHECKPOINT: Done Hello React Navigation

--- App/navigation-study/App.js
+++ App/navigation-study/App.js
@@ -13,13 +13,26 @@ function HomeScreen() {
   );
 }
 
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
 const Stack = createNativeStackNavigator();
+
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{ title: 'Overview' }}/>
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
