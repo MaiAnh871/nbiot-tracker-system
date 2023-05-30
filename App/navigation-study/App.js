c2d2033b572edd98dc0365ce69f93958ee20b103
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-30 09:39:25 +0700

    COMPLETE: Configuring the header bar

--- App/navigation-study/App.js
+++ App/navigation-study/App.js
@@ -1,52 +1,37 @@
 import * as React from 'react';
-import { Text, TextInput, View, Button } from 'react-native';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen({ navigation }) {
+function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button
-        title="Go to Profile"
-        onPress={() =>
-          navigation.navigate('Profile', { name: 'Custom profile header' })
-        }
-      />
-      <Button
-        title="Update the title"
-        onPress={() => navigation.setOptions({ title: 'Updated!' })}
-      />
-    </View>
-  );
-}
-
-function ProfileScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile screen</Text>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+        }}
+      >
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: 'My home' }}
         />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={({ route }) => ({ title: route.params.name })}
-        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
