765107dfdf4c50e7577415c5a78005e380578f3f
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-30 14:40:00 +0700

    CHECKPOINT: Adjust some UI

--- App/tracker-app/src/navigation/index.js
+++ App/tracker-app/src/navigation/index.js
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from 'react'; //useEffect to check when the component mounts
-import {View, ActivityIndicator} from 'react-native';
+import React, { useEffect, useState } from 'react'; //useEffect to check when the component mounts
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from '../screens/SignInScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import ConfirmEmailScreen from '../screens/ConfirmEmailScreen';
@@ -12,7 +12,7 @@ import NewPasswordScreen from '../screens/NewPasswordScreen';
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import RegisterDeviceScreen from '../screens/RegisterDeviceScreen';
-import {Auth, Hub} from 'aws-amplify';
+import { Auth, Hub } from 'aws-amplify';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -21,28 +21,29 @@ function TabNavigation() {
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'home'
-                : 'home-outline';
-            } else if (route.name === 'Profile') {
-              iconName = focused 
-                ? 'person' 
-                : 'person-outline';
-            } else if (route.name === 'Register Device') {
-              iconName = focused
-                ? 'add-circle'
-                : 'add-circle-outline';
-            }
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'home'
+              : 'home-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused 
+              ? 'person' 
+              : 'person-outline';
+          } else if (route.name === 'Register Device') {
+            iconName = focused
+              ? 'add-circle'
+              : 'add-circle-outline';
+          }
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={30} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: { height: 60},
+      })}
     >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Register Device" component={RegisterDeviceScreen} />
