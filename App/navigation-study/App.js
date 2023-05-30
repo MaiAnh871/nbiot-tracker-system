a4e3172e5efae99609bf1b49383b9b422e70a7b5
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-05-30 09:41:47 +0700

    CHECKPOINT: header button

--- App/navigation-study/App.js
+++ App/navigation-study/App.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function HomeScreen() {
   return (
@@ -11,23 +13,32 @@ function HomeScreen() {
   );
 }
 
-const Stack = createNativeStackNavigator();
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require('@expo/snack-static/react-native-logo.png')}
+    />
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-        }}
-      >
+      <Stack.Navigator>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'My home' }}
+          options={{
+            headerTitle: (props) => <LogoTitle {...props} />,
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Info"
+                color="#00cc00"
+              />
+            ),
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
