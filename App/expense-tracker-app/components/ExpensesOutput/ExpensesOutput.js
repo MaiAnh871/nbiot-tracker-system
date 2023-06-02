0abb6fcffcb37c934188fa7bf8f8dca529cab141
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-06-02 11:20:20 +0700

    COMPLETE: Create key app components to display expenses

---
+++ App/expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+
+function ExpensesOutput({expenses}) {
+    return (
+        <View>
+            <ExpensesSummary />
+            <ExpensesList />
+        </View>
+    );
+}
+
+export default ExpensesOutput;
---
+++ App/expense-tracker-app/components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+function ExpensesSummary() {
+    return ( 
+        <View>
+            <Text>Last 7 Days</Text>
+            <Text>$177.95</Text>
+        </View>
+    );
+}
+
+export default ExpensesSummary;
