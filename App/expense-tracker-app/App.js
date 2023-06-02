0f9506be6acef8ee6b6b9cc27c52180ae24270ac
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-06-02 10:48:15 +0700

    COMPLETE: Edit navigation config

--- App/expense-tracker-app/App.js
+++ App/expense-tracker-app/App.js
@@ -6,14 +6,49 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
+import { GlobalStyles } from './constants/styles';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview() {
-  return <BottomTabs.Navigator>
-    <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses}/>
-    <BottomTabs.Screen name="AllExpenses" component={AllExpenses}/>
+  return <BottomTabs.Navigator screenOptions={{
+    headerStyle: {backgroundColor: GlobalStyles.colors.primary500 },
+    headerTintColor: 'white',
+    tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500},
+    tabBarActiveTintColor: GlobalStyles.colors.accent500,
+  }}>
+    <BottomTabs.Screen
+      name="RecentExpenses"
+      component={RecentExpenses}
+      options={{
+        title: 'Recent Expenses',
+        tabBarLabel: 'Recent',
+        tabBarIcon: ({color, size}) => (
+          <Ionicons
+            name="hourglass"
+            size={size}
+            color={color}
+          />
+        ),
+      }}
+      />
+    <BottomTabs.Screen
+      name="AllExpenses"
+      component={AllExpenses}
+      options={{
+        title: 'All Expenses',
+        tabBarLabel: 'All Expenses',
+        tabBarIcon: ({color, size}) => (
+          <Ionicons
+            name="calendar"
+            size={size}
+            color={color}
+          />
+        ),
+      }}
+    />
   </BottomTabs.Navigator>
 }
 export default function App() {
@@ -22,7 +57,11 @@ export default function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator >
-          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen
+            name="ExpensesOverview"
+            component={ExpensesOverview}
+            options={{ headerShown: false }}
+          />
           <Stack.Screen name="ManageExpense" component={ManageExpense} /> 
         </Stack.Navigator>
       </NavigationContainer>
---
+++ App/expense-tracker-app/constants/styles.js
@@ -0,0 +1,12 @@
+export const GlobalStyles = {
+    colors: {
+        primary50: '#e4d9fd',
+        primary100: '#c6affc',
+        primary200: '#a281f0',
+        primary400: '#5721d4',
+        primary500: '#3e04c3',
+        accent500: '#f7bc0c',
+        error50: '#fcc4e4',
+        error500: '#9b095c',
+    },
+};
