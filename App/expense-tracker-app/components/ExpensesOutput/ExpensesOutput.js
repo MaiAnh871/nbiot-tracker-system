9c3e076e9998dee04f830032af4804881196c6a9
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-06-03 08:57:00 +0700

    COMPLETE: Continue working on Expense-related Component

--- App/expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
+++ App/expense-tracker-app/components/ExpensesOutput/ExpensesOutput.js
@@ -3,10 +3,10 @@ import { View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput({expenses}) {
+function ExpensesOutput({ expenses, expensesPeriod }) {
     return (
         <View>
-            <ExpensesSummary />
+            <ExpensesSummary expenses={expenses} periodName={expensesPeriod}/>
             <ExpensesList />
         </View>
     );
--- App/expense-tracker-app/components/ExpensesOutput/ExpensesSummary.js
+++ App/expense-tracker-app/components/ExpensesOutput/ExpensesSummary.js
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 
-function ExpensesSummary() {
+function ExpensesSummary( expenses, periodName ) {
+    const expensesSum = expenses.reduce((sum, expense) => {
+        return sum + expense.amount
+    }, 0);
     return ( 
         <View>
-            <Text>Last 7 Days</Text>
-            <Text>$177.95</Text>
+            <Text> {periodName} </Text>
+            <Text> ${expensesSum.toFixed(2)} </Text>
         </View>
     );
 }
