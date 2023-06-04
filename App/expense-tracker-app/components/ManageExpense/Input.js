93cb00cd8bee23df986d60bea8bd7ecae8effb34
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-06-04 20:49:37 +0700

    COMPLETE: Add styling

--- App/expense-tracker-app/components/ManageExpense/Input.js
+++ App/expense-tracker-app/components/ManageExpense/Input.js
@@ -1,12 +1,20 @@
-import { TextInput, View, Text } from "react-native";
+import { TextInput, View, Text, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function Input({ label, textInputConfig }) {
+
+    const inputStyles = [styles.input];
+
+    if (textInputConfig && textInputConfig.inputMultiline) {
+        inputStyles.push(styles.inputMultiline);
+    }
+
     return (
-        <View>
-            <Text>
+        <View style={styles.inputContainer}>
+            <Text style={styles.label}>
                 {label}
             </Text>
-            <TextInput 
+            <TextInput style = {inputStyles}
                 {...textInputConfig}
             />
         </View>
@@ -14,3 +22,26 @@ function Input({ label, textInputConfig }) {
 }
 
 export default Input;
+
+const styles=StyleSheet.create({
+    inputContainer: {
+        marginHorizontal: 4,
+        marginVertical: 8,
+    },
+    label: {
+        fontSize: 12,
+        color: GlobalStyles.colors.primary100,
+        marginBottom: 4
+    },
+    input: {
+        backgroundColor: GlobalStyles.colors.primary100,
+        padding: 6,
+        borderRadius: 6,
+        fontSize: 18,
+        color: GlobalStyles.colors.primary700
+    },
+    inputMultiline: {
+        minHeight: 100,
+        textAlignVertical: 'top'
+    }
+});
