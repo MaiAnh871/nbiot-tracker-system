443fade65decc2f0e993510d68eda5d6e71ced07
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-06-05 22:46:52 +0700

    COMPLETE: Adding a delete button

--- App/tracker-app/src/screens/ManageDevice/ManageDevice.js
+++ App/tracker-app/src/screens/ManageDevice/ManageDevice.js
@@ -2,16 +2,46 @@ import { useContext, useLayoutEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 import { DevicesContext } from '../../store/device-context';
+import IconButton from '../../components/IconButton/IconButton';
+import { GlobalStyles } from '../../constants/styles';
 
 function ManageDeviceScreen({ route }) {
     const manageDeviceId = route.params?.trackerId;
 
+    function deleteDeviceHandler() {
+
+    }
+
     return (
-        <View>
-            <Text>ManageDevice Screen</Text>
+        <View style={styles.container}>
+            <Text>Manage Device Screen</Text>
             <Text>{manageDeviceId}</Text>
+            <View style={styles.deleteContainer}>
+                <IconButton
+                    icon="trash"
+                    color="white"
+                    size = {36}
+                    onPress={deleteDeviceHandler}
+                />
+            </View>
+
         </View>
     );
   };
   
 export default ManageDeviceScreen;
+
+const styles = StyleSheet.create({
+    deleteContainer: {
+        marginTop: 60,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: "white",
+        alignItems: 'center'
+    },
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.tomato
+    }
+})
