83a13951d6400decd321155e160e365f0a68a782
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-06-15 22:57:08 +0700

    COMPLETE: Amplify push and need to cmd attach policy to a new cognito identity for pubsub

--- App/my-tracker-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ App/my-tracker-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,46 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "userInfo": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "mytrackerapp": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "userInfoDBTrigger5a887ffd": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "userInfoLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "userInfoDB": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
--- App/my-tracker-app/src/aws-exports.js
+++ App/my-tracker-app/src/aws-exports.js
@@ -3,10 +3,41 @@
 
 const awsmobile = {
     "aws_project_region": "ap-northeast-2",
-    "aws_cognito_identity_pool_id": "ap-northeast-2:35b1b555-2ef6-49b9-8e79-bcaf850d17e8",
+    "aws_dynamodb_all_tables_region": "ap-northeast-2",
+    "aws_dynamodb_table_schemas": [
+        {
+            "tableName": "userInfoTable-dev",
+            "region": "ap-northeast-2"
+        }
+    ],
+    "aws_cognito_identity_pool_id": "ap-northeast-2:2907f315-9d0e-4f4a-b794-5195c521c938",
     "aws_cognito_region": "ap-northeast-2",
-    "aws_user_pools_id": "ap-northeast-2_63M1Ynsui",
-    "aws_user_pools_web_client_id": "7r8i0ucr6k8vlq35fu5stnmg5a",
+    "aws_user_pools_id": "ap-northeast-2_Mf4uxcjL4",
+    "aws_user_pools_web_client_id": "3ica16jtohipck76fippcd0nb2",
+    "oauth": {},
+    "aws_cognito_username_attributes": [],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ],
+    "aws_cloud_logic_custom": [
+        {
+            "name": "userInfo",
+            "endpoint": "https://io9ykjmjah.execute-api.ap-northeast-2.amazonaws.com/dev",
+            "region": "ap-northeast-2"
+        }
+    ]
 };
 
 
