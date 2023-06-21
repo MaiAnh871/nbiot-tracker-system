b12c827eae4cab251243229e696fcfaa4c819a47
Duc Trung <trung.lyduc18@gmail.com>
2023-06-21 23:18:42 +0700

    COMPLETE: data format

--- Device/project_template/IP/Example/MDK_ARMv5/Core/Collections.h
+++ Device/project_template/IP/Example/MDK_ARMv5/Core/Collections.h
@@ -46,11 +46,35 @@ static struct Connection_Status {
 	int8_t sinr;
 } Connection_Status;
 
+static enum Longtitude_Direction {
+	WEST = -1,
+	EAST = 1
+} Longtitude_Direction;
+
+static struct Longtitude {
+	uint8_t degree; 	/* 0 - 180 (standardized) */
+	uint8_t minute; 	/* 0 - 60 */
+	uint8_t second;		/* 0 - 60 */
+	enum Longtitude_Direction longtitude_direction;
+} Longtitude;
+
+static enum Latitude_Direction {
+	SOUTH = -1,
+	NORTH = 1
+} Latitude_Direction;
+
+static struct Latitude {
+	uint8_t degree; 	/* 0 - 90 (standardized) */
+	uint8_t minute; 	/* 0 - 60 */
+	uint8_t second;		/* 0 - 60 */
+	enum Latitude_Direction latitude_direction;
+} Latitude;
+
 static struct Node {
 	struct Timestamp timestamp;
 	char device_id[DEVICE_ID_LENGTH];
-	char latitude[LATITUDE_LENGTH];
-	char longtitude[LONGTITUDE_LENGTH];
+	struct Latitude latitude;
+	struct Longtitude longtitude;
 	uint16_t speed; /* distance_last_node / time_interval_last_node */
 	double accel_x;
 	double accel_y;
