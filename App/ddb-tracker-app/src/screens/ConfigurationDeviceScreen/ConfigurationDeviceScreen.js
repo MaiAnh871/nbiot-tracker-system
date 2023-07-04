6b05b23fbab51661efef624b6523be12d8b62bfd
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-07-04 23:48:24 +0700

    CHECKPOINT: Fix some tiny UI

--- App/ddb-tracker-app/src/screens/ConfigurationDeviceScreen/ConfigurationDeviceScreen.js
+++ App/ddb-tracker-app/src/screens/ConfigurationDeviceScreen/ConfigurationDeviceScreen.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, TextInput, Button } from 'react-native';
+import { View, StyleSheet, TextInput, TouchableOpacity, Text } from 'react-native';
+import { GlobalStyles } from '../../constants/styles';
 
 const ConfigurationDeviceScreen = () => {
-  const [ inputValue, setInputValue ] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (text) => {
     setInputValue(text);
@@ -18,24 +19,53 @@ const ConfigurationDeviceScreen = () => {
   } 
 
   return (
-    <SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Enter the Desired Message Period Value</Text>
       <TextInput
-        style={ styles.input }
-        value={ inputValue }
-        onChangeText={ handleInputChange }
-        placeholder="Enter a value"
+        style={styles.input}
+        value={inputValue}
+        onChangeText={handleInputChange}
+        placeholder="e.g. 1 hour"
       />
-      <Button title="OK" onPress={ handleOKPress } />
-    </SafeAreaView>
+      <TouchableOpacity style={styles.button} onPress={handleOKPress}>
+        <Text style={styles.buttonText}>OK</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 50,
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
   input: {
     height: 40,
-    margin: 12,
+    width: '80%',
     borderWidth: 1,
-    padding: 10,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: GlobalStyles.colors.tomato,
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
