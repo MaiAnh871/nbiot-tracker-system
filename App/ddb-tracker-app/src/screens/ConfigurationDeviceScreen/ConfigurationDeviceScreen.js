60cf01af28a1eb7304844f070724cdfd37b4040d
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-07-04 23:41:22 +0700

    DONE: Create TextInput and its handler

--- App/ddb-tracker-app/src/screens/ConfigurationDeviceScreen/ConfigurationDeviceScreen.js
+++ App/ddb-tracker-app/src/screens/ConfigurationDeviceScreen/ConfigurationDeviceScreen.js
@@ -1,9 +1,42 @@
-import { Text } from "react-native";
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, TextInput, Button } from 'react-native';
 
-function ConfigurationDeviceScreen() {
-    return (
-      <Text>ConfigurationDeviceScreen</Text>
-    ) 
-  }
-  
-  export default ConfigurationDeviceScreen;
+const ConfigurationDeviceScreen = () => {
+  const [ inputValue, setInputValue ] = useState('');
+
+  const handleInputChange = (text) => {
+    setInputValue(text);
+  };
+
+  const handleOKPress = () => {
+    valueHandler(inputValue);
+    setInputValue('');
+  };
+
+  const valueHandler = (inputValue) => {
+    console.log(inputValue);
+  } 
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={ styles.input }
+        value={ inputValue }
+        onChangeText={ handleInputChange }
+        placeholder="Enter a value"
+      />
+      <Button title="OK" onPress={ handleOKPress } />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default ConfigurationDeviceScreen;
