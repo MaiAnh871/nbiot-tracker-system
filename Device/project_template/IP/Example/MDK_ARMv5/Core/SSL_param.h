8aef88f8aa45d8ab5528f91b5aa0e7f621bf7e24
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-07-10 11:48:19 +0700

    WORKING

---
+++ Device/project_template/IP/Example/MDK_ARMv5/Core/SSL_param.h
@@ -0,0 +1,16 @@
+#ifndef SSL_PARAM_
+#define SSL_PARAM_
+
+#ifndef CA_CERT
+#define CA_CERT ""
+#endif
+
+#ifndef CLIENT_CERT
+#define CLIENT_CERT ""
+#endif
+
+#ifndef CLIENT_KEY
+#define CLIENT_KEY ""
+#endif
+
+#endif /* SSL_PARAM_ */
